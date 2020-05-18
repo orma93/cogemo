9cf98b4ba7415cc821a0f3b7cb04189813c0670e
orma93 <orma.ravindranath@gmail.com>
2020-05-18 17:04:22 -0400

    Update MVM_resting_state_connectivity.sh

--- MVM_resting_state_connectivity.sh
+++ MVM_resting_state_connectivity.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# 3dMVM script used to run whole-brain voxelwise connectivity analysis
+# Seeded on bilateral amygdala ROI
+# Input data files are the voxelwise connectivity values computed from DoConDecon.sh
+# Controlling for sex interaction
+# Also controlling for mean frame displacement (to control for head motion)
+# Masking to include gray matter only
+# Ran 1 t-test for effect of age'
+
 3dMVM	-prefix MVM_seedconn_rest_biamy_age_gm_glt_n49.nii -jobs 40	\
 -bsVars "age*sex+meanfd"	\
 -qVars "age,meanfd"	\
