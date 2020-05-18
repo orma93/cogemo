1eccecf5b099500c06970a72d94f86c7e96fbde7
orma93 <orma.ravindranath@gmail.com>
2020-05-18 16:44:22 -0400

    Update and rename MVM_rest.sh to MVM_resting_state_connectivity.sh

--- MVM_resting_state_connectivity.sh
+++ MVM_resting_state_connectivity.sh
@@ -1,6 +1,6 @@
 3dMVM	-prefix MVM_seedconn_rest_biamy_age_gm_glt_n49.nii -jobs 40	\
--bsVars "age*sex"	\
--qVars age	\
+-bsVars "age*sex+meanfd"	\
+-qVars "age,meanfd"	\
 -mask ~/standard/mni_icbm152_nlin_asym_09c/mni_icbm152_gm_tal_nlin_asym_09c_3mm.nii	\
 -num_glt 1	\
 -gltLabel 1 age_effect -gltCode 1 'age :'	\
